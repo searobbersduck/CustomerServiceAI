52331f06b35ae29715d409d0d6849cfef0a79eb1
Higgs <higgs@weidong.local>
2019-01-18 11:52:19 +0800

    commit

--- algo/qs_matching_service.py
+++ algo/qs_matching_service.py
@@ -23,6 +23,7 @@ class ChatBotAlgoService(syntax_algo_pb2_grpc.ChatAlgoServiceServicer):
             'YOUR_TARGET':['你有什么事？','有何贵干啊?'],
             'BYE':['再见','拜拜,', '拜拜', '没啥兴趣，拜拜'],
             'CALL_LATER':['晚点再打过来', '你可以晚上再打给我吗'],
+            'FACE_TALK':['可以面谈吗', '面谈怎么样？'],
             'NO_MATCH': ['你怎么知道', '你怎么知道我在找工作', '你怎么知道我手机号', '你怎么知道我联系方式的']
         }
         self.choice_list_raw = []
