b382a796b046b6c007c659eb6a9c12a763f35e4a
Higgs <higgs@weidong.local>
2019-06-24 16:46:32 +0800

    commit

---
+++ asr/asr_correction/lm_client.py
@@ -0,0 +1,59 @@
+# !/usr/bin/env python3
+
+from grpc.beta import implementations
+import numpy as np
+import tensorflow as tf
+
+from tensorflow_serving.apis import predict_pb2
+from tensorflow_serving.apis import prediction_service_pb2
+
+import sys
+sys.path.append('./bert')
+import tokenization
+import numpy as np
+import fire
+
+server = '172.16.52.70:8500'
+
+host, port = server.split(':')
+#
+# channel = implementations.insecure_channel(host, int(port))
+#
+# stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
+#
+# request = predict_pb2.PredictRequest()
+#
+# request.model_spec.name = 'saved_model'
+
+
+def get_connection(host, port, model_name):
+    host, port = server.split(':')
+    channel = implementations.insecure_channel(host, int(port))
+    stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
+    request = predict_pb2.PredictRequest()
+    request.model_spec.name = 'saved_model'
+    return request, stub
+
+def get_input_data(text_a, max_len, tokenizer):
+    tokens_a = tokenizer.tokenize(text_a)
+    tokens_a = ['<S>'] + tokens_a + ['<T>']
+    ids_a = tokenizer.convert_tokens_to_ids(tokens_a)
+    x_data = np.zeros([max_len], dtype=np.int32)
+    len_max = min(max_len, len(ids_a))
+    x_data[:len_max] = ids_a
+    y_data = [len_max]
+    return x_data, y_data
+
+request, stub = get_connection(host, int(port), 'saved_model')
+
+tokenizer = tokenization.FullTokenizer('./vocab.txt')
+
+def get_score(text_a, max_len, tokenizer=tokenizer):
+    x_data, y_data = get_input_data(text_a, max_len, tokenizer)
+    request.inputs['inp'].CopyFrom(tf.contrib.util.make_tensor_proto(x_data, shape=[1, 64]))
+    request.inputs['inp_len'].CopyFrom(tf.contrib.util.make_tensor_proto(y_data, shape=[1]))
+    result = stub.Predict(request, 10.0)
+    print(result)
+
+if __name__ == '__main__':
+    fire.Fire()
---
+++ asr/asr_correction/lm_model_to_tfserver.py
@@ -0,0 +1,28 @@
+# !/usr/bin/env python3
+
+import tensorflow as tf
+import os
+import fire
+
+def convert_tfmodel_to_tfservermodel(meta_file, ckpt_file, version):
+    graph = tf.Graph()
+    with graph.as_default():
+        with tf.Session() as sess:
+            saver = tf.train.import_meta_graph(meta_file)
+            saver.restore(sess, ckpt_file)
+            tvs = [v for v in tf.trainable_variables()]
+            # for v in tvs:
+            #     print(v.name)
+            # print(graph.get_tensor_by_name('lm_score:0'))
+            # print(graph.get_operation_by_name('inp'))
+            score = graph.get_tensor_by_name('lm_score:0')
+            inp = graph.get_tensor_by_name('inp:0')
+            inp_len = graph.get_tensor_by_name('inp_len:0')
+            # score_val = sess.run(score, {inp: np_inp, inp_len: np_len})
+            # print(score_val)
+            tf.saved_model.simple_save(sess, version, inputs={'inp': inp, 'inp_len': inp_len}, outputs={'score': score})
+            # tf.train.Saver().save(sess, './sss')
+            print('save tf serving model finished!')
+
+if __name__ == '__main__':
+    fire.Fire()
